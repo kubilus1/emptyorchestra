a4a50fda51448d83665ae371364e3bfe399be160
mattkubilus <mattkubilus@acaa7340-7e46-11de-b8ac-f508f1a2864c>
2009-10-15 22:20:53 +0000

    Move the list widgets into a separate module.  Disable the Timer since Windows does not like it and it currently does nothing.

--- emptyorch.py
+++ emptyorch.py
@@ -22,257 +22,11 @@ import emptyorch_xrc
 from pycdg import cdgPlayer
 from pykconstants import *
 
+from eo_widgets import Playlist_list 
+
 APPDIR = sys.path[0]
 if os.path.isfile(APPDIR): 
   APPDIR = os.path.dirname(APPDIR)
-
-class SortVirtList(
-    wx.ListCtrl, 
-    listmix.ListCtrlAutoWidthMixin, 
-    listmix.ColumnSorterMixin,
-    ):
-
-
-    headers = []
-    rows = []
-    itemDataMap = {}
-
-    def __init__(self):
-        p = wx.PreListCtrl()
-        self.PostCreate(p)
-        self.Bind(wx.EVT_WINDOW_CREATE, self.OnCreate)
-
-    def OnCreate(self, event):
-        self.attr1 = wx.ListItemAttr()
-        self.attr1.SetBackgroundColour("white")
-        listmix.ListCtrlAutoWidthMixin.__init__(self)
-        listmix.ColumnSorterMixin.__init__(self, 20)
-
-    def SearchData(self, term):
-        index = -1
-        searchData = {}
-        for row in self.rows:
-            found = False
-            for col in row:
-                if term.lower() in col.lower():
-                    found = True
-                    break
-            if found:
-                index += 1
-                searchData[index] = row
-        self.itemDataMap = searchData
-        self.itemIndexMap = self.itemDataMap.keys()
-        self.SetItemCount(len(self.itemDataMap))
-
-    def ClearSearch(self):
-        self.itemDataMap = {}
-
-        for i in range(0, len(self.rows)):
-            self.itemDataMap[i] = self.rows[i]
-
-        self.itemIndexMap = self.itemDataMap.keys()
-        self.SetItemCount(len(self.itemDataMap))
-
-    def SetData(self, headers, rows):
-        self.ClearAll()
-        self.headers = headers
-        self.rows.extend(rows)
-
-        for i in range(len(headers)):
-            self.InsertColumn(i, headers[i])
-            self.SetColumnWidth(i, wx.LIST_AUTOSIZE)
-        
-        start = len(self.itemDataMap) - 1
-        end = start + len(rows)
-        for i in range(start, end):
-            self.itemDataMap[i] = self.rows[i]
-
-        self.itemIndexMap = self.itemDataMap.keys()
-        self.SetItemCount(len(self.itemDataMap))
-
-    def SaveData(self, filename):
-        print "Saving %s" % filename
-        f = open(filename, 'w')
-        try:
-            pickle.dump((self.headers, self.rows), f)
-        finally:
-            f.close()
-
-    def LoadData(self, filename):
-        if os.path.isfile(filename):
-            print "Loading %s" % filename
-            f = open(filename)
-            try:
-                headers, rows = pickle.load(f)
-                self.SetData(headers, rows)
-            finally:
-                f.close()
-
-    def OnColClick(self,event):
-        event.Skip()
-
-    def OnItemActivated(self, event):
-        self.currentItem = event.m_itemIndex
-
-    def getColumnText(self, index, col):
-        item = self.GetItem(index, col)
-        return item.GetText()
-
-    def OnItemDeselected(self, evt):
-        pass
-
-    #---------------------------------------------------
-    # These methods are callbacks for implementing the
-    # "virtualness" of the list...
-
-    def OnGetItemText(self, item, col):
-        index=self.itemIndexMap[item]
-        s = self.itemDataMap[index][col]
-        return s
-
-    def OnGetItemAttr(self, item):
-        if item % 2 == 1:
-            return self.attr1
-        else:
-            return None
-
-
-    #---------------------------------------------------
-    # Matt C, 2006/02/22
-    # Here's a better SortItems() method --
-    # the ColumnSorterMixin.__ColumnSorter() method already handles the ascending/descending,
-    # and it knows to sort on another column if the chosen columns have the same value.
-
-    def SortItems(self,sorter=cmp):
-        print "SortItems"
-        items = list(self.itemDataMap.keys())
-        items.sort(sorter)
-        self.itemIndexMap = items
-        
-        # redraw the list
-        self.Refresh()
-
-    # Used by the ColumnSorterMixin, see wx/lib/mixins/listctrl.py
-    def GetListCtrl(self):
-        return self
-
-
-class EditMediaList(SortVirtList, listmix.TextEditMixin):
-    def OnCreate(self, event):
-        listmix.TextEditMixin.__init__(self)
-        SortVirtList.OnCreate(self, event)
-
-    def SetVirtualData(self, item, col, data):
-        #SortVirtList.SetVirtualData(self, item, col, data)
-        #self.SetStringItem(self, item, col, data)
-        index=self.itemIndexMap[item]
-        self.itemDataMap[index][col] = data
-        artist = self.itemDataMap[index][0]
-        title = self.itemDataMap[index][1]
-        genre = self.itemDataMap[index][2]
-        mtype = self.itemDataMap[index][3]
-        path = self.itemDataMap[index][4]
-
-        if mtype == 'mp3':
-            m = MP3(path, ID3=EasyID3)
-            try:
-                m.add_tags(ID3=EasyID3)
-            except mutagen.id3.error:
-                print "Already has tag"
-        elif mtype == 'ogg':
-            m = OggVorbis(path)
-        else:
-            print "Unrecognized type."
-            return
-
-        m['title'] = title
-        m['artist'] = artist
-        m['genre'] = genre
-        m.save()
-        print "Updated data."
-
-class Playlist_list(gizmos.EditableListBox):
-#class Playlist_list(wx.ListCtrl):
-    def __init__(self, *args, **kwds):
-        gizmos.EditableListBox.__init__(self, *args, **kwds)
-
-    def SetData(self, headers, rows):
-        self.ClearAll()
-        self.headers = headers
-        self.rows.extend(rows)
-
-        for i in range(len(headers)):
-            self.InsertColumn(i, headers[i])
-            self.SetColumnWidth(i, wx.LIST_AUTOSIZE)
-        
-        start = len(self.itemDataMap) - 1
-        end = start + len(rows)
-        for i in range(start, end):
-            self.itemDataMap[i] = self.rows[i]
-
-        self.itemIndexMap = self.itemDataMap.keys()
-        self.SetItemCount(len(self.itemDataMap))
-
-    def addToList(self, artist, title, filename, archive):
-        strings = self.GetStrings()
-        strings.append("  |  ".join((artist, title, filename, archive)))
-        self.SetStrings(strings)
-
-    def getCurrent(self):
-        index = -1
-        playlistCtrl = self.GetListCtrl()
-        index = playlistCtrl.GetNextItem(
-            index,
-            state=wx.LIST_STATE_SELECTED
-        )
-        data = playlistCtrl.GetItem(index).GetText()
-        print "Got: ", data
-        return data.split("  |  ")
-    
-    def selectNext(self):
-        index = -1
-        playlistCtrl = self.GetListCtrl()
-        numItems = playlistCtrl.GetItemCount()
-        index = playlistCtrl.GetNextItem(
-            index,
-            state=wx.LIST_STATE_SELECTED
-        )
-        next = (index + 1) % numItems
-        playlistCtrl.SetItemState(
-                index, 
-                0,
-                wx.LIST_STATE_SELECTED
-        )
-        playlistCtrl.SetItemState(
-                next, 
-                wx.LIST_STATE_SELECTED,
-                wx.LIST_STATE_SELECTED
-        )
-
-    def selectPrev(self):
-        index = -1
-        playlistCtrl = self.GetListCtrl()
-        numItems = playlistCtrl.GetItemCount()
-        index = playlistCtrl.GetNextItem(
-            index,
-            state=wx.LIST_STATE_SELECTED
-        )
-
-        if index > 0:
-            prev = index - 1
-        else:
-            prev = numItems - 1
-
-        playlistCtrl.SetItemState(
-                index, 
-                0,
-                wx.LIST_STATE_SELECTED
-        )
-        playlistCtrl.SetItemState(
-                prev, 
-                wx.LIST_STATE_SELECTED,
-                wx.LIST_STATE_SELECTED
-        )
 
 class MyApp(wx.App):
 
@@ -288,6 +42,9 @@ class MyApp(wx.App):
         self.init_frame()
         return True
 
+    #def __del__(self):
+    #    self.timer.Stop()
+
     def init_frame(self):
         # Get stuff from the XRC
         self.frm = self.res.LoadFrame(None, 'emptyorch_frame') 
@@ -299,7 +56,7 @@ class MyApp(wx.App):
         self.st_file = xrc.XRCCTRL(self.frm, 'st_file')
         self.file_tree = xrc.XRCCTRL(self.frm, 'file_tree')
         self.media_list = xrc.XRCCTRL(self.frm, 'media_list')
-        self.queue_list = xrc.XRCCTRL(self.frm, 'queue_list')
+        #self.queue_list = xrc.XRCCTRL(self.frm, 'queue_list')
 
         # Attach an unknown wxSearchCtrl
         self.search = wx.SearchCtrl(self.frm, -1, "", style=wx.TE_PROCESS_ENTER)
@@ -309,6 +66,11 @@ class MyApp(wx.App):
         self.Bind(wx.EVT_MENU, self.OnMenu_open_menu, id=xrc.XRCID('open_menu'))
         self.Bind(wx.EVT_BUTTON, self.OnButton_choose_btn, id=xrc.XRCID('choose_btn'))
         self.Bind(wx.EVT_BUTTON, self.OnButton_playsel_btn, id=xrc.XRCID('playsel_btn'))
+        self.Bind(
+            wx.EVT_LIST_ITEM_ACTIVATED, 
+            self.OnButton_playsel_btn,
+            id=xrc.XRCID('media_list')
+        )
         self.Bind(wx.EVT_BUTTON, self.OnButton_addplay_btn, id=xrc.XRCID('addplay_btn'))
         self.Bind(wx.EVT_SCROLL, self.OnScroll_slider, id=xrc.XRCID('slider'))
         self.Bind(wx.EVT_BUTTON, self.OnButton_prev_btn, id=xrc.XRCID('prev_btn'))
@@ -330,9 +92,9 @@ class MyApp(wx.App):
             self.Destroy()
             raise
 
-        self.st_size = wx.StaticText(self.frm, 0, size=(100,-1))
-        self.st_len  = wx.StaticText(self.frm, -1, size=(100,-1))
-        self.st_pos  = wx.StaticText(self.frm, -1, size=(100,-1))
+        #self.st_size = wx.StaticText(self.media_panel, 0, size=(100,-1))
+        #self.st_len  = wx.StaticText(self.media_panel, -1, size=(100,-1))
+        #self.st_pos  = wx.StaticText(self.media_panel, -1, size=(100,-1))
     
         # Setup the Playlist
         sizer = wx.BoxSizer(wx.VERTICAL)
@@ -347,9 +109,9 @@ class MyApp(wx.App):
         sizer.Add(self.playlist, 1, wx.EXPAND)
         self.playlist_panel.SetSizer(sizer)
 
-        self.timer = wx.Timer(self)
-        self.Bind(wx.EVT_TIMER, self.onTimer)
-        self.timer.Start(100)
+        #self.timer = wx.Timer(self)
+        #self.Bind(wx.EVT_TIMER, self.onTimer)
+        #self.timer.Start(100)
 
         self.media_list.LoadData('.musicdata')
         self.frm.Show()
@@ -478,9 +240,9 @@ class MyApp(wx.App):
     def onTimer(self, evt):
         offset = self.mc.Tell()
         self.slider.SetValue(offset)
-        self.st_size.SetLabel('size: %s ms' % self.mc.Length())
-        self.st_len.SetLabel('( %d seconds )' % (self.mc.Length()/1000))
-        self.st_pos.SetLabel('position: %d ms' % offset)
+        #self.st_size.SetLabel('size: %s ms' % self.mc.Length())
+        #self.st_len.SetLabel('( %d seconds )' % (self.mc.Length()/1000))
+        #self.st_pos.SetLabel('position: %d ms' % offset)
 
     def OnScroll_slider(self, evt):
         offset = self.slider.GetValue()
