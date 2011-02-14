2da85f2fe1336a46d9e55ea3a3969fa71b058274
mattkubilus <mattkubilus@acaa7340-7e46-11de-b8ac-f508f1a2864c>
2011-02-14 01:30:23 +0000

    Name the directory based on OS

--- bfreeze.py
+++ bfreeze.py
@@ -1,8 +1,9 @@
+import os
 import shutil
 
 from bbfreeze import Freezer
 
-f = Freezer("emptyorch_rel")
+f = Freezer("emptyorch_%s" % os.name)
 f.addScript("emptyorch.py")
 f()
 shutil.copy("emptyorch.xrc", "emptyorch_rel")
