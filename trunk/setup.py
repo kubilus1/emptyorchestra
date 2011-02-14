64752bbcb3d8a1178d06a19b017dc7969c115dcd
mattkubilus <mattkubilus@acaa7340-7e46-11de-b8ac-f508f1a2864c>
2011-02-14 01:23:25 +0000

    Add missing lines from setup.py

--- trunk/setup.py
+++ trunk/setup.py
@@ -4,6 +4,8 @@ setup(
 name='emptyorchestra',
 version='0.0.2',
 author='Matt Kubilus',
+author_email="kubilus1@yahoo.com",
+url="www.kubilus.com/emptyorch",
 packages=['emptyorchestra'],
 package_dir = {'emptyorchestra':''},
 package_data = {'emptyorchestra':['*.xrc']},
